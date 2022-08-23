9d9ab16e5b27b3bfb3806cd767a94d251eb8c5ab
Brandontmh <s10222989@connect.np.edu.sg>
2022-08-24 02:20:49 +0800

    Search bar

--- script.js
+++ script.js
@@ -14,14 +14,14 @@ if (close){
     })
 }
 
-const search = () =>{
-    const searchbox = document.getElementById("search-item").ariaValueMax.toUpperCase();
-    const storeitems = document.getElementsByClassName(".pro-container")
+const search = () => {
+    const searchbox = document.getElementById("search-item").value.toUpperCase();
+    const storeitems = document.getElementById("pro-container")
     const product = document.querySelectorAll(".pro")
     const pname = storeitems.getElementsByTagName("h5")
 
-    for(var i=0; i< pname.length; i++){
-        let match = product[i].getElementsByTagName("h2")[0];
+    for(var i=0; i < pname.length; i++){
+        let match = product[i].getElementsByTagName('h5')[0];
 
         if(match) {
             let textvalue = match.textContent || match.innerHTML
